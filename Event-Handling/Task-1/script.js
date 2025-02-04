e5beacc9cb8660993b45ebc485dc327806f49204
Thomas Brakutt <thomas.brakutt@gmail.com>
2025-02-04 18:49:05 +0100

    creat folder and files for Task1

---
+++ Event-Handling/Task-1/script.js
@@ -0,0 +1,17 @@
+Vue.createApp({
+  data() {
+    return {
+      x: 12,
+      y: 4,
+      fruitBasket: [
+        "🍏 Apple",
+        "🍌 Banana",
+        "🍉 Melon",
+        "🫐 Blueberry",
+        "🍓 Strawberry",
+        "🍍 Ananas",
+        "🥭 Mango",
+      ],
+    };
+  },
+}).mount("#app");
