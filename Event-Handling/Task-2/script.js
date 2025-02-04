c2d5ad2979b297f7881229a339d9bbd66d361e38
Thomas Brakutt <thomas.brakutt@gmail.com>
2025-02-04 20:54:57 +0100

    code cleaning

--- Event-Handling/Task-2/script.js
+++ Event-Handling/Task-2/script.js
@@ -14,4 +14,80 @@ Vue.createApp({
       ],
     };
   },
+
+  methods: {
+    removeFruit(fruitToRemove) {
+      // Neues Array erstellen, das nur die Früchte enthält, die nicht entfernt werden sollen
+      let updatedFruitBasket = [];
+
+      // Durch das ursprüngliche Array iterieren
+      for (let i = 0; i < this.fruitBasket.length; i++) {
+        // Die aktuelle Frucht
+        let fruit = this.fruitBasket[i];
+
+        // Wenn die aktuelle Frucht NICHT die zu entfernende Frucht ist,
+        // wird sie zum neuen Array hinzugefügt
+        if (fruit !== fruitToRemove) {
+          updatedFruitBasket.push(fruit);
+        }
+      }
+
+      // Das alte Array wird durch das neue ersetzt
+      this.fruitBasket = updatedFruitBasket;
+    },
+  },
 }).mount("#app");
+
+/* 
+advanced Alternativen:
+
+Variante von Joe:
+------------------
+
+methods: {
+  removeFruit(fruitToRemove) {
+    this.fruitBasket = this.fruitBasket.filter((fruit) => {
+      return fruit !== fruitToRemove;
+    });
+  }
+}
+
+andere Alternative:
+-------------------
+
+methods: {
+  removeFruit(fruitToRemove) {
+    // 'filter' geht jedes Element des Arrays durch und gibt ein neues Array zurück.
+    // Die Funktion im 'filter' prüft, ob das Element behalten werden soll.
+    this.fruitBasket = this.fruitBasket.filter(function(fruit) {
+      // Wenn die Frucht nicht die zu entfernende Frucht ist, wird sie behalten.
+      return fruit !== fruitToRemove;
+    });
+  }
+}
+
+mit forEach-Schleife:
+----------------------
+
+methods: {
+  removeFruit(fruitToRemove) {
+    // Neues Array erstellen, das nur die Früchte enthält, die nicht entfernt werden sollen
+    let neuesFruchtArray = [];
+
+    // Durch das ursprüngliche Array iterieren mit forEach
+    this.fruitBasket.forEach(function(fruit) {
+      // Wenn die aktuelle Frucht NICHT die zu entfernende Frucht ist,
+      // wird sie zum neuen Array hinzugefügt
+      if (fruit !== fruitToRemove) {
+        neuesFruchtArray.push(fruit);
+      }
+    });
+    
+    // Das alte Array wird durch das neue ersetzt
+    this.fruitBasket = neuesFruchtArray;
+  }
+}
+
+
+
+*/
